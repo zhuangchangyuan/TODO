f27a80d3793054d2720cd0e2a823bedf42f39291
zcy <zcy973526@sina.com>
2019-07-04 16:32:44 +0800

    encode\decode读取数据提交修改

--- TODO/Controllers/TodoListViewController.swift
+++ TODO/Controllers/TodoListViewController.swift
@@ -9,22 +9,27 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    var itemArray = ["购买水杯", "吃药", "修改密码"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
-
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = items
-        }
+        
+         
+        loadItems()
         // Do any additional setup after loading the view.
     }
     //MARK: - Table View DataSource methods
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // print("更新第：\(indexPath.row)行")
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
-        
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        if itemArray[indexPath.row].done == false {
+            cell.accessoryType = .none
+        }else {
+            cell.accessoryType = .checkmark
+        }
         
         
         return cell
@@ -34,11 +39,11 @@ class TodoListViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-             tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else {
-             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = itemArray[indexPath.row].done
+        saveItems()
+        
+        
+      
        
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -47,8 +52,12 @@ class TodoListViewController: UITableViewController {
         var textField = UITextField()
         let alert = UIAlertController(title: "添加一个新的ToDo项目", message: "",preferredStyle: .alert)
         let action = UIAlertAction(title: "添加项目", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+           self.saveItems()
+                
             self.tableView.reloadData()
             
         }
@@ -59,6 +68,26 @@ class TodoListViewController: UITableViewController {
         
         alert.addAction(action)
         present(alert,animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        }catch {
+            print("编码错误：\(error)")
+        }
+    }
+    func loadItems()  {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }catch {
+                print("解码错!")
+            }
+        }
     }
 }
 
---
+++ TODO/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  TODO
+//
+//  Created by 庄昌元 on 2019/7/4.
+//  Copyright © 2019 庄昌元. All rights reserved.
+//
+
+import Foundation
+class Item:Codable{
+    var title = ""
+    var done = false
+}
